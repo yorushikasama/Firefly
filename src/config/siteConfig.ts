8111f260a3ff6868a068884a78d44ecd449acd41
yorushikasama <1371356392@qq.com>
2026-09-22 08:18:30 +0800

    chore(site): 站点标题改为 Yuzuriha Inori

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -42,7 +42,7 @@ const pages = resolvePageToggles({
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "楪祈",
+	title: "Yuzuriha Inori",
 
 	// 站点副标题
 	subtitle: "Yuzuriha Inori",
@@ -118,7 +118,7 @@ export const siteConfig: SiteConfig = {
 			alt: "🍀",
 		},
 		// 导航栏标题
-		title: "楪祈",
+		title: "Yuzuriha Inori",
 		// 全宽导航栏，导航栏是否占满屏幕宽度
 		widthFull: false,
 		// 导航菜单对齐方式，left：左对齐，center：居中
